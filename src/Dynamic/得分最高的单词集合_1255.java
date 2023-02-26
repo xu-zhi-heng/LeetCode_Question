170919ccb9053ad15b075ea1d4829fe145daadef
xu-zhi-heng <3116322768@qq.com>
2023-02-26 12:38:31 +0800

    2023/02/26 by SweetFun

---
+++ src/Dynamic/得分最高的单词集合_1255.java
@@ -0,0 +1,7 @@
+package Dynamic;
+
+public class 得分最高的单词集合_1255 {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+
+    }
+}
---
+++ src/Greedy/交换字符使得字符串相同_1247.java
@@ -0,0 +1,21 @@
+package Greedy;
+
+public class 交换字符使得字符串相同_1247 {
+    public int minimumSwap(String s1, String s2) {
+        int xy = 0, yx = 0;
+        int n = s1.length();
+        for (int i = 0; i < n; i++) {
+            char a = s1.charAt(i), b = s2.charAt(i);
+            if (a == 'x' && b == 'y') {
+                xy++;
+            }
+            if (a == 'y' && b == 'x') {
+                yx++;
+            }
+        }
+        if ((xy + yx) % 2 == 1) {
+            return -1;
+        }
+        return xy / 2 + yx / 2 + xy % 2 + yx % 2;
+    }
+}
