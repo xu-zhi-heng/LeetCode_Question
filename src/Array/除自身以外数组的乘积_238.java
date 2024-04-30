c934a27f38e49ca1cc1e8ceb9a08b93219e8236b
徐志恒 <3116322768@qq.com>
2024-04-30 11:10:12 +0800

    2024/04/30

--- src/Array/除自身以外数组的乘积_238.java
+++ src/Array/除自身以外数组的乘积_238.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.Arrays;
+
 /**
  * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
  *
@@ -9,7 +11,24 @@ package Array;
  */
 public class 除自身以外数组的乘积_238 {
     public static void main(String[] args) {
-        productExceptSelf(new int[]{1,2,3,4});
+        int[] nums = new int[]{1,2,3,4};
+        int[] L = new int[nums.length];
+        int[] R = new int[nums.length];
+        L[0] = 1;
+        for(int i = 1; i < nums.length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+        System.out.println(Arrays.toString(L));
+        R[nums.length - 1] = 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+        System.out.println(Arrays.toString(R));
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = L[i] * R[i];
+        }
+        System.out.println(Arrays.toString(ans));
     }
     public static int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
